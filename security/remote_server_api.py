14fa47b8594c7e1a785c45213f67b25dc649320d
liutianbao1 <liutianbao1@faw.com.cn>
2024-07-23 16:58:26 +0800

    fix: 更新远端登录逻辑

--- security/remote_server_api.py
+++ security/remote_server_api.py
@@ -5,34 +5,44 @@ e.g. provide email and password, send this information to your remote server
 waiting for the response from remote server. If remote server authorized this email and password
 then it will be ok to login this user.
 """
+import base64
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+import requests
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-def authenticate(username, password):
+def authenticate(email, psw):
     """
 
-    :param username:
-    :param password:
+    :param email:
+    :param psw:
     :return:
     """
-    server_url = "https://yourloginservice.com"
-    auth_url = '%s/auth' % server_url
-    param_dict = dict(login=username, password=password)
-    headers = "prepare your data"
+    public_key = 'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBANL378k3RiZHWx5AfJqdH9xRNBmD9wGD2iRe41HdTNF8RUhNnHit5NpMNtGL0NPTSSpPjjI1kJfVorRvaQerUgkCAwEAAQ=='
+    cipher_public = PKCS1_v1_5.new(RSA.importKey(base64.b64decode(public_key)))
+    encrypt_psw = base64.b64encode(cipher_public.encrypt(psw.encode())).decode()
+    server_url = "http://fawivi-sit-k8-operatingweb.faw.cn"
+    auth_url = f'{server_url}/rbac/auth/login'
+    post_data = {
+        "platId": "303191694492971008",
+        "userAccount": email,
+        "userPassword": encrypt_psw
+    }
+    headers = {}
     # this is an example of real world case
-    auth_result = requests.post(
-        auth_url, json=param_dict, headers=headers, verify=False)
-    if auth_result.status_code != 200:
+    response = requests.post(auth_url, json=post_data, headers=headers, verify=False)
+    if response.status_code != 200:
         logger.warn("failed to auth user: %s, status: %s, response: %s " %
-                    (username, auth_result.status_code, auth_result.text))
+                    (email, response.status_code, response.text))
         return None
-    result_dict = auth_result.json()
-    userinfo = dict(
-        username=username)
-    return userinfo
+    response_json = response.json()
+    if response_json['statusCode'] != '0':
+        print(response_json)
+        logger.warn(f'post api data failed. url: {auth_url}.')
+        return None
 
-    # mock the server response here
-    if username == 'admin@faw.com.cn':
-        userinfo = dict(username=username)
-        return userinfo
-    else:
-        return None
+    userinfo = dict(email=email)
+    return userinfo
